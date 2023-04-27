0c31d8769c819a21b239805515b5ef2b80836bfe
solenwa <solen.wanono@gmail.com>
2023-04-27 11:29:10 +0200

    Show and artist models added

---
+++ models/Artist.model.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+
+const artistSchema = new Schema(
+    {
+        name: {
+          type: String,
+          required: true,
+        },
+        bio: {
+          type: String,
+          required: true
+        },
+        upcomingShows: {
+            type: [String],
+            required: true
+        },
+        pastShows: {
+            type: [String],
+            required: true,
+        },
+        newsSubscription: {
+            type: boolean,
+            required: true
+        },
+      },
+    {
+        // this second object adds extra properties: `createdAt` and `updatedAt`    
+        timestamps: true
+      }
+);
+
+const Artist = model("Artist", artistSchema);
+    
+module.exports = Artist;
+    
