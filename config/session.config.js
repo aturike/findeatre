8a2a6c73e33430f83d571b92b393ccce92fde82c
Adam Turi <aturike@gmail.com>
2023-05-03 09:52:19 +0200

    extra data Paris shows

--- config/session.config.js
+++ config/session.config.js
@@ -21,7 +21,7 @@ module.exports = (app) => {
         sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         secure: process.env.NODE_ENV === "production",
         httpOnly: true,
-        maxAge: 60000,
+        maxAge: 60 * 60 * 24,
       }, // ADDED code below !!!
       store: MongoStore.create({
         mongoUrl:
